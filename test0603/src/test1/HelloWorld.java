f5d09d9a1cf2191231dd699d5d9140d8a8fc15fb
lbo_duyuqiang@126.com <lbo_duyuqiang@126.com>
2021-06-06 09:32:23 +0800

    java

--- test0603/src/test1/HelloWorld.java
+++ test0603/src/test1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello People");
         System.out.println("Hello People2");
+        System.out.println("Hello World2");
     }
 }
